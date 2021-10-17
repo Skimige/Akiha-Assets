1e65035941efc4100d6ef885be8be994244f4942
Skimige <me@xyx.moe>
2021-10-17 19:47:06 +0800

    Clean up (again)

--- fetch.sh
+++ fetch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-curl $WAHLAP_SHOP_API_URL -o shop_data.json > /dev/null
-wget "$ALIAS_SHEET_TSV_URL" -O aliases.tsv > /dev/null
-git config --global user.name 'Skimige'
-git config --global user.email 'skimige@users.noreply.github.com'
+curl $WAHLAP_SHOP_API_URL -o shop_data.json > /dev/null 2>&1
+wget "$ALIAS_SHEET_TSV_URL" -O aliases.tsv > /dev/null 2>&1
+git config --global user.name "$GITHUB_ACTOR"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git commit -a -m "Update data"
 if [ $? -eq 0 ]; then
   git push
