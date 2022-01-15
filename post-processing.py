4ed1117347b0e5bfe8a39bd4cbe032f1f51f86bf
Skimige <me@xyx.moe>
2022-01-15 21:00:52 +0800

    ci: add post-processing script

---
+++ post-processing.py
@@ -0,0 +1,13 @@
+import json
+
+with open('shop_data.json', 'r', -1, 'utf_8') as f:
+    shops = json.load(f)
+    shops_ht = {}
+    for i in shops:
+        i_id = i['id']
+        shops_ht[i_id] = i
+        del shops_ht[i_id]['id']
+        del shops_ht[i_id]['placeId']
+
+with open('shop_data.json', 'w', -1, 'utf_8') as f:
+    json.dump(shops_ht, f, indent=4, ensure_ascii=False)
