7d8506e90b5e81bb7d2d4c93682c055aea79bb96
Skimige <me@xyx.moe>
2022-01-16 13:50:48 +0800

    ci: fix dupe .json() and encoding of alias

--- retrieve-data.py
+++ retrieve-data.py
@@ -20,13 +20,14 @@ with open('data/shop_data.json', 'w', -1, 'utf-8') as f:
     json.dump(shop_data_ht, f, indent=4, ensure_ascii=False)
 
 # Alias
+alias_data.encoding = 'utf-8'
 with open('data/aliases.tsv', 'w', -1, 'utf-8') as f:
     f.write(alias_data.text)
 
 # Music
 music_data = {i['id']: i for i in music_data}
 with open('data/music_data.json', 'w', -1, 'utf-8') as f:
-    json.dump(music_data.json(), f, indent=4, ensure_ascii=False)
+    json.dump(music_data, f, indent=4, ensure_ascii=False)
 
 # Chart Stats
 with open('data/chart_stats.json', 'w', -1, 'utf-8') as f:
