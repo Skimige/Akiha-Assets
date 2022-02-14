2e84d39fc3dc285fd17a7a7cbe9d0f2afbf85209
Skimige <me@xyx.moe>
2022-02-14 20:02:09 +0800

    Rename & Add Arcaea positioninfo.json

--- retrieve-data.py
+++ retrieve-data.py
@@ -16,19 +16,19 @@ for i in shop_data:
     shop_data_ht[i_id] = i
     del shop_data_ht[i_id]['placeId']
 
-with open('data/shop_data.json', 'w', -1, 'utf-8') as f:
+with open('data/maimai/shop_data.json', 'w', -1, 'utf-8') as f:
     json.dump(shop_data_ht, f, indent=4, ensure_ascii=False)
 
 # Alias
 alias_data.encoding = 'utf-8'
-with open('data/aliases.tsv', 'w', -1, 'utf-8') as f:
+with open('data/maimai/aliases.tsv', 'w', -1, 'utf-8') as f:
     f.write(alias_data.text)
 
 # Music
 music_data = {i['id']: i for i in music_data}
-with open('data/music_data.json', 'w', -1, 'utf-8') as f:
+with open('data/maimai/music_data.json', 'w', -1, 'utf-8') as f:
     json.dump(music_data, f, indent=4, ensure_ascii=False)
 
 # Chart Stats
-with open('data/chart_stats.json', 'w', -1, 'utf-8') as f:
+with open('data/maimai/chart_stats.json', 'w', -1, 'utf-8') as f:
     json.dump(chart_stats_data, f, indent=4, ensure_ascii=False)
