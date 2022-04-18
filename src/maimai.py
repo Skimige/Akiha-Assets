9c2bb4bf0d5198321893e7d95f96890d602e8930
Skimige <me@xyx.moe>
2022-04-18 18:22:30 +0800

    Dummy push

--- src/maimai.py
+++ src/maimai.py
@@ -7,6 +7,9 @@ from flask_restful import Resource
 with open(os.path.join('..', 'data', 'maimai', 'music_data.json'), 'r', -1, 'utf-8') as f:
     music_data = json.load(f)
 
+with open(os.path.join('..', 'data', 'maimai', 'chart_stats.json'), 'r', -1, 'utf-8') as f:
+    chart_stats = json.load(f)
+
 
 class MaimaiDXMusicInfo(Resource):
     def get(self, id: str):
