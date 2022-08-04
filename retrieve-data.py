dd8e5a1662de1e41560d1561bd624a303cdb5e1b
Skimige <me@xyx.moe>
2022-08-04 12:55:54 +0800

    Add Chunithm

--- retrieve-data.py
+++ retrieve-data.py
@@ -3,21 +3,23 @@ import json
 
 import requests
 
+# Shop Data
+games = ['chunithm', 'maimai']
+for game in games:
+    shop_data = requests.get(os.getenv(f'WAHLAP_{game.upper()}_SHOP_DATA_URL')).json()
+    shop_data_ht = {}
+    for i in shop_data:
+        i_id = i['id']
+        shop_data_ht[i_id] = i
+        del shop_data_ht[i_id]['placeId']
 
-shop_data = requests.get(os.getenv('WAHLAP_SHOP_API_URL')).json()
+    with open(f'data/{game}/shop_data.json', 'w', -1, 'utf-8') as f:
+        json.dump(shop_data_ht, f, indent=4, ensure_ascii=False)
+
+# maimai
 alias_data = requests.get(os.getenv('ALIAS_SHEET_TSV_URL'))
 music_data = requests.get(os.getenv('MUSIC_DATA_URL')).json()
 chart_stats_data = requests.get(os.getenv('CHART_STATS_DATA_URL')).json()
-
-# Shop Data
-shop_data_ht = {}
-for i in shop_data:
-    i_id = i['id']
-    shop_data_ht[i_id] = i
-    del shop_data_ht[i_id]['placeId']
-
-with open('data/maimai/shop_data.json', 'w', -1, 'utf-8') as f:
-    json.dump(shop_data_ht, f, indent=4, ensure_ascii=False)
 
 # Alias
 alias_data.encoding = 'utf-8'
